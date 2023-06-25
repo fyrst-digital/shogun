dd2aa3ce46db21fb062eb46d7f697a8f52e94ffa
jinroh <robsenq@gmail.com>
2023-06-25 17:08:00 +0200

    add build method

--- src/FyrstShogun.php
+++ src/FyrstShogun.php
@@ -3,6 +3,7 @@
 namespace FyrstShogun;
 
 use Shopware\Core\Framework\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
@@ -10,4 +11,8 @@ use Shopware\Storefront\Framework\ThemeInterface;
 
 class FyrstShogun extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+    }
 }
