05f71aabed7f7601f0f4a63b4d9495684b791d90
Robert <robsenq@gmail.com>
2024-07-21 18:39:15 +0200

    improve gallery-slider component

--- src/Resources/app/storefront/src/js/component/gallery-slider.js
+++ src/Resources/app/storefront/src/js/component/gallery-slider.js
@@ -12,16 +12,22 @@ export default class GallerySlider extends Plugin {
         canvasContainerSelector: '.gallery-slider-canvas-container',
         canvasSelector: '[data-sh-gallery-slider="canvas"]',
         thumbnailsSelector: '[data-sh-gallery-slider="thumbnails"]',
+        thumbnailSelector: '[data-sh-gallery-slider="thumbnail"]',
         nextSelector: '[data-sh-slider-control="next"]',
         prevSelector: '[data-sh-slider-control="prev"]'
     }
 
     init() {
+        this.activeSlide = 0
         this.canvasContainerElement = this.el.querySelector(this.options.canvasContainerSelector)
         this.canvasElement = this.el.querySelector(this.options.canvasSelector)
         this.canvasNextButton = this.canvasContainerElement.querySelector(this.options.nextSelector)
         this.canvasPrevButton = this.canvasContainerElement.querySelector(this.options.prevSelector)
         
+        this.thumbnailsElement = this.el.querySelector(this.options.thumbnailsSelector)
+        this.thumbnailElements = this.thumbnailsElement.querySelectorAll(this.options.thumbnailSelector)
+        this.setThumbnailActive(this.activeSlide)
+
         this.canvasSlider = new Swiper(this.canvasElement, {
             loop: false,
             rewind: true,
@@ -35,6 +41,27 @@ export default class GallerySlider extends Plugin {
             this.canvasSlider.slidePrev();
         })
 
-        console.log(this.canvasNextButton, this.canvasSlider)
+        this.thumbnailElements.forEach((thumbnail, index) => {
+            thumbnail.addEventListener("click", (e) => {
+                this.canvasSlider.slideTo(index)
+            })
+        });
+
+        this.canvasSlider.on("slideChange", (slider) => {
+            this.resetThumbnailsActiveClass()
+            this.setThumbnailActive(slider.activeIndex)
+        })
+    }
+
+    setThumbnailActive(index) {
+        this.activeSlide = index
+        this.activeThumbnail = this.thumbnailElements.item(index)
+        this.activeThumbnail.classList.add("active")
+    }
+
+    resetThumbnailsActiveClass() {
+        this.thumbnailElements.forEach((thumbnail) => {
+            thumbnail.classList.remove("active")
+        })
     }
 }
